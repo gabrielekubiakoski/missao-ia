dd355a582fe4c817979719d3a4ee2fbe8f10904f
GABRIELI KUBIAKOSKI <108832324+gabrielekubiakoski@users.noreply.github.com>
2025-11-10 09:34:44 -0300

    123456

---
+++ java/aleatorio.js
@@ -0,0 +1,6 @@
+const nomes = ["Gabrieli","Eduardo","Angela","Rodrigo","Poliana"];
+export function aleatorio (lista){
+    const posicao = Math.floor(Math.random()*lista.lenght);
+    returnlista [posicao];
+}
+export const nome = aleatorio(nomes)
---
+++ java/perguntas.js
@@ -0,0 +1,97 @@
+export const perguntas = [
+    {
+    enunciado:"",
+    alternativas:[
+        {
+            texto:"",
+            afirmacao:[
+                "."
+            ]
+            proxima: 1,
+        },
+        {
+            texto:"",
+            afirmacao:[ 
+                ""
+            ],
+            proxima: 2,
+        }
+    ]
+},
+{
+    enunciado:"",
+    alternativas:[
+        {
+            texto:"", 
+            afirmacao:[
+                ""
+            ],
+            proxima:3,
+        },
+        {
+            texto:"",
+            afirmacao: [
+                  ""
+            ],
+            proxima: 4,
+        }
+    ]
+},
+{
+    enunciado:
+    alternativas:[
+        {
+            texto:
+            afirmacao:[
+
+            ],
+            proxima:3,
+        },
+        {
+            texto:
+            afirmacao: [
+
+            ],
+            proxima: 4,
+        }
+    ]
+},
+{
+    enunciado:
+    alternativas:[
+        {
+            texto:
+            afirmacao:[
+
+            ],
+            proxima:3,
+        },
+        {
+            texto:
+            afirmacao: [
+
+            ],
+            proxima: 4,
+        }
+    ]
+},
+{
+    enunciado:
+    alternativas:[
+        {
+            texto:
+            afirmacao:[
+
+            ],
+            proxima:3,
+        },
+        {
+            texto:
+            afirmacao: [
+
+            ],
+            proxima: 4,
+        }
+    ]
+},
+]
