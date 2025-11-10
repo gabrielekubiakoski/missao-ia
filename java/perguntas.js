2d8c04b616115c713959857ae631f4a417d41e56
GABRIELI KUBIAKOSKI <108832324+gabrielekubiakoski@users.noreply.github.com>
2025-11-10 09:51:12 -0300

    1234567ui

--- java/perguntas.js
+++ java/perguntas.js
@@ -1,18 +1,18 @@
 export const perguntas = [
     {
-    enunciado:"",
+    enunciado:"As redes sociais fazem parte da vida cotidiana de milhões de pessoas, oferecendo oportunidades de interação, aprendizado e entretenimento. No entanto, o uso excessivo e a busca constante por aprovação virtual podem causar efeitos negativos na saúde mental, como ansiedade, dependência emocional e baixa autoestima. Diante disso, é importante refletir sobre o modo como utilizamos essas plataformas e o impacto que elas exercem sobre nosso bem-estar psicológico.Qual é a melhor forma de utilizar as redes sociais de maneira saudável e equilibrada?",
     alternativas:[
         {
-            texto:"",
+            texto:"Utilizar as redes sociais de forma consciente, estabelecendo limites de tempo, evitando comparações e priorizando conteúdos que transmitam mensagens positivas e construtivas.",
             afirmacao:[
-                "."
+                "Essa atitude é favorável porque promove o uso equilibrado das redes, ajudando a preservar a saúde mental. Ao limitar o tempo online e consumir conteúdos inspiradores, o indivíduo reduz a exposição a comparações negativas e diminui o risco de desenvolver ansiedade e insatisfação pessoal."
             ]
             proxima: 1,
         },
         {
-            texto:"",
+            texto:"Usar as redes sociais constantemente, buscando reconhecimento por meio de curtidas e comentários, mesmo que isso gere ansiedade e desgaste emocional.",
             afirmacao:[ 
-                ""
+              "Essa escolha é desfavorável porque estimula a dependência da validação externa e pode gerar frustração quando as expectativas de aprovação não são atendidas. O uso excessivo também aumenta a exposição a críticas e comparações, fatores que contribuem para o estresse e a perda de autoestima."
             ],
             proxima: 2,
         }
