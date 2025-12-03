371b8c330972b4b398e85c1a4b92b8efada60607
GABRIELI KUBIAKOSKI <108832324+gabrielekubiakoski@users.noreply.github.com>
2025-12-03 14:34:25 -0300

    12345678

--- java/script.js
+++ java/script.js
@@ -51,7 +51,7 @@ function mostraAfirmacoes (){
 }
 
 function respostaSelecionada(opcaoSelecionada) {
-    const afirmacoes = aleatorio(opcaoSelecionada.afirmacoes);
+    const afirmacoes = aleatorio(opcaoSelecionada.afirmacao);
     historiaFinal += afirmacoes + " ";
     if (opcaoSelecionada.proxima !== undefined) {
         atual = opcaoSelecionada.proxima;
